829060c6404bd550ea1a83a8f192a9166a57d144
Richard Hull <rm_hull@yahoo.co.uk>
2016-07-09 19:56:17 +0100

    Move test dataset into separate namespace

--- test/clustering/test_helper.clj
+++ test/clustering/test_helper.clj
@@ -20,31 +20,42 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
-(ns clustering.qt-test
+(ns clustering.test-helper
   (:require
     [clojure.test :refer :all]
-    [clustering.qt :refer :all]
-    [clustering.test-helper :refer :all]
-    [clj-time.core :refer [date-time]]))
+    [clj-time.core :refer [after? date-time interval in-days]]
+    [clj-time.format :refer [unparse formatters]]))
 
-(deftest check-candidate-cluster
-  (is (empty? (candidate-cluster distance (date-time 2013 7 15) test-dataset 0)))
-  (is (empty? (candidate-cluster distance (date-time 2013 7 15) nil 150)))
-  (is (= (candidate-cluster distance (date-time 2013 7 15) test-dataset 1)
-         (hash-set (date-time 2013 7 14))))
-  (is (= (candidate-cluster distance (date-time 2013 7 15) test-dataset 15)
-         (hash-set
-           (date-time 2013 7 1)
-           (date-time 2013 7 14)
-           (date-time 2013 7 21)
-           (date-time 2013 7 25))))
-  (is (= (candidate-cluster distance (date-time 2013 7 15) test-dataset 31)
-         (hash-set
-           (date-time 2013 7 1)
-           (date-time 2013 7 14)
-           (date-time 2013 7 21)
-           (date-time 2013 7 25)
-           (date-time 2013 7 31)
-           (date-time 2013 8 3)))))
+(defn distance [dt-a dt-b]
+  (if (after? dt-a dt-b)
+    (distance dt-b dt-a)
+    (in-days (interval dt-a dt-b))))
 
+(def fmt (partial unparse (formatters :date)))
 
+(def test-dataset
+  (hash-set
+    (date-time 2013 7 21)
+    (date-time 2013 7 25)
+    (date-time 2013 7 14)
+    (date-time 2013 7 31)
+    (date-time 2013 7 1)
+    (date-time 2013 8 3)
+
+    (date-time 2012 12 26)
+    (date-time 2012 12 28)
+    (date-time 2013 1 16)
+
+    (date-time 2012 6 2)
+    (date-time 2012 6 7)
+    (date-time 2012 6 6)
+    (date-time 2012 6 9)
+    (date-time 2012 5 28)))
+
+(deftest check-distance
+  (is (= 7 (distance (date-time 2013 7 21) (date-time 2013 7 28))))
+  (is (= 7 (distance (date-time 2013 7 28) (date-time 2013 7 21))))
+  (is (= 0 (distance (date-time 2016 1 19) (date-time 2016 1 19)))))
+
+(deftest check-fmt
+  (is (= "2019-02-19" (fmt (date-time 2019 2 19)))))
