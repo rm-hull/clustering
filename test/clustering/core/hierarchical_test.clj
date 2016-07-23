80fbcb74ebb77a3d1acba7316c700945aa3e5ec9
Richard Hull <rm_hull@yahoo.co.uk>
2016-07-23 21:14:56 +0100

    Hierarchical clustering tests

--- test/clustering/core/hierarchical_test.clj
+++ test/clustering/core/hierarchical_test.clj
@@ -23,45 +23,39 @@
 (ns clustering.core.hierarchical-test
   (:require
     [clojure.test :refer :all]
-    [clojure.test.check :as tc]
-    [clojure.test.check.generators :as gen]
-    [clojure.test.check.properties :as prop]
-    [clojure.test.check.clojure-test :refer [defspec]]
     [clustering.core.hierarchical :refer :all]
-    [clustering.data-viz.dendrogram :refer :all]
-    [clustering.data-viz.image :refer :all]
-    [clustering.test-helper :refer :all]
-    [clj-time.core :refer [date-time]]))
+    [clustering.average.simple :refer :all]
+    [clustering.distance.euclidean :refer :all]))
 
-(defn print-clusters
-  ([clust]
-   (print-clusters clust 0))
+(def test-data
+  (map vector [-1 7 98 22 14 19 21 3]))
 
-  ([clust n]
-   (dotimes [i n]
-     (print " "))
+(deftest check-find-closest
+  (is (= [] (find-closest distance nil)))
+  (is (= [1.0 [[22] [21]]] (find-closest distance test-data))))
 
-   (if (:branch? clust)
-     (println "-")
-     (println (:distance clust) "###" (fmt (:data clust))))
+(deftest check-cluster-and-walk
+  (let [collector (atom [])
+        visitor (fn [clust lvl]
+                  (swap! collector conj
+                         [lvl (or (:branch? clust) (:data clust))]))]
+    (->>
+      (cluster distance average test-data)
+      (prefix-walk visitor))
 
-   (when-let [left (:left clust)]
-     (print-clusters left (inc n)))
-
-   (when-let [right (:right clust)]
-     (print-clusters right (inc n)))))
-
-(comment
-  (def groups (cluster distance average test-dataset))
-  (print-clusters groups)
-
-  (write-png
-    "doc/dendrogram.png"
-    (->img groups fmt))
-
-  (spit
-    "doc/dendrogram.svg"
-    (->svg groups fmt))
-)
-
-
+    (is (= [[0 true]
+            [1 true]
+            [2 true]
+            [3 true]
+            [4 [-1]]
+            [4 [3]]
+            [3 [7]]
+            [2 true]
+            [3 true]
+            [4 true]
+            [5 [22]]
+            [5 [21]]
+            [4 [19]]
+            [3 [14]]
+            [1 [98]]]
+           @collector))))
