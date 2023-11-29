7b52d740021c437511afddeb733c1f34369237ee
Richard Hull <rm_hull@yahoo.co.uk>
2023-11-29 23:55:53 +0000

    Update dependencies & add github action to deploy API docs

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
-    [org.clojure/math.combinatorics "0.1.5"]
+    [org.clojure/math.combinatorics "0.2.0"]
     [rm-hull/helpmate "0.1.4"]]
   :scm {:url "git@github.com:rm-hull/clustering.git"}
   :source-paths ["src"]
@@ -21,17 +21,17 @@
       "LICENSE.md"
     ]
     :output-path "doc/api"
-    :source-uri "http://github.com/rm-hull/clustering/blob/master/{filepath}#L{line}"  }
+    :source-uri "http://github.com/rm-hull/clustering/blob/main/{filepath}#L{line}"  }
   :min-lein-version "2.8.1"
   :profiles {
     :dev {
       :global-vars {*warn-on-reflection* true}
       :dependencies [
-        [org.clojure/clojure "1.9.0"]
-        [org.clojure/data.csv "0.1.4"]
-        [org.clojure/test.check "0.9.0"]
-        [clj-time "0.15.1"]]
+        [org.clojure/clojure "1.11.1"]
+        [org.clojure/data.csv "1.0.1"]
+        [org.clojure/test.check "1.1.1"]
+        [clj-time "0.15.2"]]
       :plugins [
-        [lein-codox "0.10.7"]
-        [lein-cljfmt "0.6.4"]
-        [lein-cloverage "1.1.1"]]}})
+        [lein-codox "0.10.8"]
+        [lein-cljfmt "0.9.2"]
+        [lein-cloverage "1.2.4"]]}})
